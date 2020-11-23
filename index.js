466822322387754217b8cfc43fc8a2df31efbb70
Naka Masato <nakamasato1989@gmail.com>
2020-11-23 10:44:24 +0900

    fix

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const ref = github.context.ref
+  console.log(`ref: ${ref}`);
   console.log("--------------------------------");
   console.log(`The event payload: ${payload}`);
 } catch (error) {
