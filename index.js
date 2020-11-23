c91d025fb97d8aecad880356b2c7e799d28266d0
Naka Masato <nakamasato1989@gmail.com>
2020-11-23 10:31:10 +0900

    check base branch

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const baseBranch = github.context.payload.base.ref
+  console.log(`The base branch: ${baseBranch}`);
+  console.log("--------------------------------");
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
